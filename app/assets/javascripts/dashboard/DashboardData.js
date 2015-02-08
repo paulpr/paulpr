0df5a2c3558c421d6bf024ffa4b04d1dfa0d19b0
Chris Jaure <chrisjaure@gmail.com>
2015-02-07 19:30:32 -0600

    add more realistic chart data

--- app/assets/javascripts/dashboard/DashboardData.js
+++ app/assets/javascripts/dashboard/DashboardData.js
@@ -4,25 +4,26 @@ import {Line} from 'react-chartjs';
 var data = {
   labels: ["January", "February", "March", "April", "May", "June", "July"],
   datasets: [
-    {
-      label: "My First dataset",
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
+	{
+      label: "Pull Requests",
+      fillColor: "rgba(100,100,100,0.4)",
+      strokeColor: "rgba(100,100,100,1)",
+      pointColor: "rgba(100,100,100,1)",
       pointStrokeColor: "#fff",
+      strokeWidth: 2,
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: [65, 59, 80, 81, 56, 55, 40]
+      pointHighlightStroke: "rgba(100,100,100,1)",
+      data: [28, 29, 24, 19, 54, 18, 21]
     },
     {
-      label: "My Second dataset",
-      fillColor: "rgba(151,187,205,0.2)",
-      strokeColor: "rgba(151,187,205,1)",
-      pointColor: "rgba(151,187,205,1)",
+      label: "Comments by Paul",
+      fillColor: "rgba(220,10,10,0.2)",
+      strokeColor: "rgba(220,10,10,1)",
+      pointColor: "rgba(220,10,10,1)",
       pointStrokeColor: "#fff",
       pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)",
-      data: [28, 48, 40, 19, 86, 27, 90]
+      pointHighlightStroke: "rgba(220,10,10,1)",
+      data: [65, 59, 47, 31, 43, 13, 15]
     }
   ]
 };
@@ -41,13 +42,34 @@ export default class DashboardData extends React.Component {
 }
 
 class PullRequestTimeline extends React.Component{
-	componentDidMount() {
-		this.refs.lineChart.generateLegend();
-	}
 	render() {
-		var options = { legendTemplate : "<div>test</div>" };
+		var options = {
+			multiTooltipTemplate: "<%= value %> <%= datasetLabel %>",
+			datasetStrokeWidth : 2,
+		};
+		var legendStyle = {
+			display: 'inline-block',
+			marginRight: 20
+		};
+		var blockStyle = function(color) {
+			return {
+				backgroundColor: color,
+				display: 'inline-block',
+				width: '1em',
+				height: '1em',
+				marginRight: 2,
+				verticalAlign: -3
+			}
+		};
+		var legend = data.datasets.map(dataset => {
+			return <span style={legendStyle}><span style={blockStyle(dataset.fillColor)}></span>{dataset.label}</span>;
+		});
 		return (
-			<Line ref="lineChart" data={this.props.data} options={options} width="600" height="250"/>
+			<div>
+				<h2>Pull Requests vs Issues</h2>
+				<Line ref="lineChart" data={this.props.data} options={options} width="600" height="250"/>
+				<div style={{ textAlign: 'center', padding: 10, boxShadow: 'inset 0 0 5px rgba(0,0,0,0.2)', width: 600, backgroundColor: '#fffbe2' }}>{legend}</div>
+			</div>
 		);
 	}
 }
@@ -59,7 +81,6 @@ class PullRequestCounter extends React.Component{
   }
 
   componentDidMount(){
-    console.log(this);
     var loop = ()=> {
       setTimeout(()=> {
             this.setState({count: this.state.count + 1});
