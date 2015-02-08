4fa771f4d3aea65cccc4fa8bebc871bb95be6712
Chris Schmitz <ccschmitz@gmail.com>
2015-02-08 11:21:00 -0600

    Fix sign out link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 
 var React = require('react');
 var DashboardData = require('./dashboard/DashboardData');
