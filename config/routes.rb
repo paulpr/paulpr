820ac2de59cdb60aeac56786a1ee04003f928cb2
Chris Schmitz <ccschmitz@gmail.com>
2015-02-08 10:51:52 -0600

    Add GitHub authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'dashboards#index'
   get '/dashboard/styleguide', to: 'dashboards#styleguide'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
   resource :github_webhooks, only: :create, defaults: { formats: :json }
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
 end
